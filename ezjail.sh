6b478f63762f7516048fa4cb74be3ffe05f75d4e
cryx <cryx>
2010-01-04 12:08:54 +0000

    Remove a debug echo.

--- ezjail.sh
+++ ezjail.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: ezjail.sh,v 1.48 2009/12/29 12:26:00 cryx Exp $
+# $Id: ezjail.sh,v 1.49 2010/01/04 12:08:54 cryx Exp $
 #
 # $FreeBSD$
 #
@@ -95,7 +95,6 @@ do_cmd()
     # Get the JID of the jail
     [ -f "/var/run/jail_${ezjail_safename}.id" ] && ezjail_id=`cat /var/run/jail_${ezjail_safename}.id` || return
 
-    echo ${ezjail_id}
     # Attach ZFS-datasets to the jail
     for zfs in ${ezjail_zfs_datasets}; do
       /sbin/zfs jail ${ezjail_id} ${zfs} ||Êecho -n "Error: ${zfs} could not be configured"
